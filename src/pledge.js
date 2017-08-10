46e079ac70ffa9046e197e7ae84fc0c143f11710
derrickbeining <derrick.beining@gmail.com>
2017-08-10 01:27:53 -0400

    ch. 4 specs all passed

--- src/pledge.js
+++ src/pledge.js
@@ -4,93 +4,158 @@ Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
-function $Promise(executor) {
-
-  if (typeof executor !== 'function') throw new TypeError('Type error: Promise constructor invoked with no executor function.');
-
+function $Promise ( executor ) {
   this._state = 'pending';
-  this._value = undefined;
-
-  this._internalResolve = (value) => {
-    if (this._state === 'pending') {
-      this._state = 'fulfilled';
-      this._value = value;
-    }
-    // console.log('I am "this":', this);
-    this._callHandlers()
-  }
-
-  this._internalReject = (reason) => {
-    if (this._state === 'pending') {
-      this._state = 'rejected';
-      this._value = reason;
-    }
-    this._callHandlers();
-  }
-
-  this._callHandlers = () => {
-    while (this._handlerGroups.length) {
-      let cb;
-
-      if (this._state === 'fulfilled') {
-        cb = this._handlerGroups.shift();
-
-        if (cb.successCb) {
-          cb.successCb(this._value);
-        }
-
-        // return cb.downstreamPromise;
-
-      } else if (this._state === 'rejected') {
-        cb = this._handlerGroups.shift();
-
-        if(cb.errorCb){
-        cb.errorCb(this._value);
-        }
-      }
-    }
-  }
-
   this._handlerGroups = [];
-
-  this.then = (onSuccess, onReject) => {
-    if (typeof onSuccess !== 'function') onSuccess = null;
-    if (typeof onReject !== 'function') onReject = null;
-
-    const newPromise = new $Promise(() => {
-
-    })
-
-    this._handlerGroups.push({
-      successCb: onSuccess,
-      errorCb: onReject,
-      downstreamPromise: newPromise
-    });
-    if (this._state !== 'pending' ) {
-      this._callHandlers();
-    }
-
-  }
-
-  this.catch = function(onReject) {
-    return this.then(null, onReject);
-  }
-
-  // this.catch = this.then.bind(this,null)
-
-  const resolve = this._internalResolve;
-  const reject = this._internalReject;
-  executor(resolve, reject);
-
+  executor(
+    this._internalResolve.bind( this ),
+    this._internalReject.bind( this )
+  )
 }
 
-const promisedValue = new Promise(function (resolve, reject) {
-  someAsynOperation('some argument', function (err, result) {
-    if (err) reject(err);
-    else resolve(result);
-  })
-})
+$Promise.prototype._settle = function ( state, value ) {
+  if ( this._isSettled() ) return;
+  this._state = state;
+  this._value = value;
+  this._callHandlers();
+}
 
+$Promise.prototype._internalResolve = function ( value ) {
+  this._settle( 'fulfilled', value );
+}
+
+$Promise.prototype._internalReject = function ( reason ) {
+  this._settle( 'rejected', reason );
+}
+
+$Promise.prototype.then = function ( onSuccess, onReject ) {
+  debugger;
+  if ( !isFn( onSuccess ) ) onSuccess = null;
+  if ( !isFn( onReject ) ) onReject = null;
+
+  const newPromise = new $Promise( noop )
+  const newHandlerGroup = {
+    successCb: onSuccess,
+    errorCb: onReject,
+    downstreamPromise: newPromise
+  };
+  this._handlerGroups.push( newHandlerGroup );
+
+  if ( this._isSettled() ) this._callHandlers();
+  return newPromise;
+}
+
+$Promise.prototype._callHandlers = function () {
+  debugger;
+  if ( this._isPending() ) return; // no need to run if Promise has no val
+  this._handlerGroups.forEach( group => {
+    debugger;
+    const handler = this._getCorrectHandler( group ); // fulfill/reject handler
+    const nextPromise = group.downstreamPromise;
+    if ( !handler ) return this._propogateTo( nextPromise );
+    let whatWasReturned;
+    try { whatWasReturned = handler( this._value ); }
+    catch ( err ) { nextPromise._internalReject( err ); }
+    if ( whatWasReturned instanceof $Promise ) {
+      nextPromise._assimilate( whatWasReturned );
+    } else {
+      nextPromise._internalResolve( whatWasReturned );
+    }
+  } );
+  this._clearHandlerQueue();
+}
+
+// this is equivalent to commented-out version below, but better
+$Promise.prototype._assimilate = function (aPromise) {
+  aPromise.then(
+    this._internalResolve.bind(this),
+    this._internalReject.bind(this)
+  );
+};
+
+// MY ORIGINAL ATTEMPT:
+// $Promise.prototype._assimilate = function ( promise2 ) {
+//   const promise1 = this;
+//   promise2.then(
+//     function ( result ) { promise1._internalResolve( result ) },
+//     function ( reason ) { promise1._internalReject( reason ) }
+//   );
+// }
+
+$Promise.prototype._clearHandlerQueue = function () {
+  this._handlerGroups = [];
+}
+
+$Promise.prototype.catch = function ( onReject ) {
+  return this.then( null, onReject );
+}
+// this.catch = this.then.bind(this,null)
+
+$Promise.prototype._getCorrectHandler = function ( handlers ) {
+  return this._isFulfilled()
+    ? handlers.successCb
+    : handlers.errorCb;
+}
+
+$Promise.prototype._propogateTo = function ( nextPromise ) {
+  return this._isFulfilled()
+    ? nextPromise._internalResolve( this._value )
+    : nextPromise._internalReject( this._value );
+}
+
+$Promise.prototype._isPending = function () {
+  return this._state === 'pending';
+}
+
+$Promise.prototype._isSettled = function () {
+  return this._state !== 'pending';
+}
+
+$Promise.prototype._isFulfilled = function () {
+  return this._state === 'fulfilled';
+}
+
+$Promise.prototype._isRejected = function () {
+  return this._state === 'rejected';
+}
+
+function noop () { };
+function isFn ( val ) { return typeof val === 'function' };
+
+
+
+// const promisedValue = new Promise(function (resolve, reject) {
+//   someAsynOperation('some argument', function (err, result) {
+//     if (err) reject(err);
+//     else resolve(result);
+//   })
+// })
+
+
+  // while (this._handlerGroups.length) {
+  //   let handler = this._handlerGroups.shift();
+  //   let cb =
+  //   if (handler[cb]) {
+  //     handler[cb](this._value);
+  //   } else {
+
+  //     handler.downstreamPromise._internalResolve()
+  //   }
+  //   if (this._isFulfilled()) {
+  //     handler = this._handlerGroups.shift();
+
+  //     if (handler.successCb) {
+  //       handler.successCb(this._value);
+  //     }
+
+  //   } else if (this._isRejected()) {
+  //     handler = this._handlerGroups.shift();
+
+  //     if(handler.errorCb){
+  //     handler.errorCb(this._value);
+  //     }
+  //   }
+  // }
 
 
 
